e2ab39f41696efb20c70d1835221e01ddabe1391
Albert Pratomo <albertpratomo21@gmail.com>
2025-04-03 00:22:03 +0200

    feat: undo sticky sections

--- pages/[[slug]].vue
+++ pages/[[slug]].vue
@@ -6,31 +6,25 @@ const recipient: Recipient | undefined = typeof slug === 'string'
     : undefined
 
 provide(recipientInjectionKey, recipient)
-
-const { y } = useWindowScroll()
-const stickyIntro = computed(() => y.value < 1300)
 </script>
 
 <template>
     <main class="font-serif max-w-lg mx-auto">
         <TheMenu />
 
-        <Intro
-            class="z-[7] sticky bottom-0"
-            :class="{ 'top-0': stickyIntro }"
-        />
+        <Intro class="sticky top-0" />
 
-        <Letter class="z-[8]" />
+        <Letter class="" />
 
-        <OurStory class="z-[8] relative" />
+        <OurStory class="relative" />
 
-        <Gallery class="z-[8]" />
+        <Gallery class="relative" />
 
-        <Venue class="z-[3] sticky bottom-0" />
+        <Venue class="relative" />
 
-        <Wishes class="z-[2] sticky bottom-0" />
+        <Wishes class="relative" />
 
-        <Gift class="z-[1] sticky bottom-0" />
+        <Gift class="relative" />
 
         <Music />
     </main>
