de72e2351049abe30621ce3ea248f600c6c2d7d3
Albert Pratomo <albertpratomo21@gmail.com>
2025-04-03 02:24:53 +0200

    feat: update seo data

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useSeoMeta({
+    title: 'Albert & Melissa',
+    ogTitle: 'Albert & Melissa',
+    description: 'The wedding of Albert & Melissa',
+    ogDescription: 'The wedding of Albert & Melissa',
+    ogImage: 'https://a.storyblok.com/f/210339/2048x1365/a4bd776a3e/a-m-04406.jpg/m/1200x630',
+    twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
     <NuxtPage />
 </template>
